226638986ea7ebcff418cc6ed5a38ffe9ea5bf67
WYGUI <wenyuangui@yeah.net>
2020-05-08 10:03:26 +0800

    feat: sonata client (#6)
    * feat: generate MEF Sonata API model and client
    
    * feat: split notify api to different directory

--- go.mod
+++ go.mod
@@ -3,6 +3,12 @@ module github.com/iixlabs/virtual-lsobus
 go 1.13
 
 require (
+	github.com/go-openapi/errors v0.19.4
+	github.com/go-openapi/runtime v0.19.15
+	github.com/go-openapi/strfmt v0.19.5
+	github.com/go-openapi/swag v0.19.9
+	github.com/go-openapi/validate v0.19.8
+	github.com/pkg/errors v0.9.1 // indirect
 	go.uber.org/multierr v1.5.0 // indirect
 	go.uber.org/zap v1.10.0
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0
---
+++ sonata/generate-api.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# Client
+## Serviceability
+### Address
+swagger generate client -t ./address -f ./spec/MEF_api_geographicAddressManagement_3.0.0.json
+### Site
+swagger generate client -t ./site -f ./spec/MEF_api_geographicSiteManagement_3.0.0.json
+### POQ
+swagger generate client -t ./poq -f ./spec/MEF_api_productOfferingQualificationManagement_3.0.1.json
+
+## Quote
+swagger generate client -t ./quote -f ./spec/MEF_api_quoteManagement_2.0.0.json
+
+## Product Order
+swagger generate client -t ./order/ -f ./spec/MEF_api_productOrderManagement_3.0.1.json
+
+## Product Inventory
+swagger generate client -t ./inventory -f ./spec/MEF_api_productInventoryManagement_3.0.0.json
+
+## Product Specification
+swagger generate model -t ./common -f ./spec/MEF_api_productSpec.json
+
+# Server
+## POQ Notification
+swagger generate server --exclude-main --skip-support --exclude-spec -t ./notify/poq -f ./spec/MEF_api_productOfferingQualificationNotification_3.0.0.json
+
+## Quote Notification
+swagger generate server --exclude-main --skip-support --exclude-spec -t ./notify/quote -f ./spec/MEF_api_quoteNotification_1.0.0.yaml
+
+## Product Order Notification
+swagger generate server --exclude-main --skip-support --exclude-spec -t ./notify/order/ -f ./spec/MEF_api_productOrderNotification_3.0.0.json
